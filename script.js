f255a8cd8d1cd934a77420451e39f23cc079853c
Rotich <jane.rotich@student.moringaschool.com>
2024-01-07 16:01:11 +0300

    create basic website

---
+++ script.js
@@ -0,0 +1,3 @@
+const h2 = document.createElement("h2");
+h2.textContent = "I love the software dveloper that I am becoming, Kudos girl, May God see you through";
+document.querySelector("body").appendChild(h2);
